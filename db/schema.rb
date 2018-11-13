8cfcf89472b341a233ddd5234d9f2e4d699adf42
Harley Rayner <harleymteden@gmail.com>
2018-11-13 18:46:36 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.decimal "price"
     t.string "size"
     t.text "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "haunted_houses", force: :cascade do |t|
