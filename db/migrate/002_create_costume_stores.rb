959d037171f265e0f13a9653b85671f1f7cb7ea1
Harley Rayner <harleymteden@gmail.com>
2018-11-13 18:19:00 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,18 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :costume_stores do |costume_store|
+      costume_store.string :name
+      costume_store.decimal :location
+      costume_store.string :inventory
+      costume_store.text :employee_count
+      costume_store.text :open_or_closed
+      costume_store.text :opening_time
+      costume_store.text :closing_time
+      costume_store.timestamps
+    end
+  end
+
+end
